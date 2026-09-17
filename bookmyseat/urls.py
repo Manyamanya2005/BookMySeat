63b842e3e67887eeb0dc2593efddde8c139585ce
Manya J <manya@email.com>
2026-09-17 10:43:00 +0530

    Fix production media images

--- bookmyseat/urls.py
+++ bookmyseat/urls.py
@@ -5,17 +5,18 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("users.urls")),
+
+    path("", include("movies.urls")),
     path("movies/", include("movies.urls")),
+    path("", include("users.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
 
-    urlpatterns += static(
-        settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT
-    )
+urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
